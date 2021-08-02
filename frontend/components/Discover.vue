e2422fc351d2b8db319bbb9afee2f4ff7b3f6bbd
vvcaw <57096338+vvcaw@users.noreply.github.com>
2021-08-02 17:49:55 +0200

    Working but not with grid with 2 cols

--- frontend/components/Discover.vue
+++ frontend/components/Discover.vue
@@ -1,14 +1,14 @@
 <template>
   <div class="bg-black h-screen grid grid-cols-10 items-center justify-center">
-    <div ref="no" class="bg-black border-r-2 border-dashed col-span-2 h-full w-full">
+    <div v-on:mouseenter="toggleActiveState(0)" ref="no" class="bg-black border-r-2 border-dashed col-span-2 h-full w-full">
 
     </div>
-    <div class="flex items-center col-span-6 justify-center">
-      <div ref="draggable" class="cursor-move p-4 bg-white border border-opacity-25 h-64 w-64 rounded-md">
+    <div class="flex w-full h-full items-center col-span-6 justify-center">
+      <div ref="draggable" class="transition-all duration-700 ease-out cursor-move p-4 bg-white border border-opacity-25 h-72 w-64 rounded-md">
         <h1 class="text-white">Some head</h1>
       </div>
     </div>
-    <div ref="yes" class="bg-black border-l-2 border-dashed col-span-2 h-full w-full">
+    <div v-on:mouseenter="toggleActiveState(1)" ref="yes" class="bg-black border-l-2 border-dashed col-span-2 h-full w-full">
 
     </div>
   </div>
@@ -18,112 +18,21 @@
 
 export default {
   name: "Discover",
-  methods: {},
   data() {
     return {
       direction: 0
     }
   },
-  mounted() {
-    let card = this.$refs.draggable
-    let yes = this.$refs.yes
-    let no = this.$refs.no
+  methods: {
+    toggleActiveState(direction) {
+      let card = this.$refs.draggable
 
-    $(yes).droppable({
-      drop: (event, ui) => {
-        console.log("Yes!")
+      if (direction === 0) {
+        $(card).css({'transform': 'rotate(-10deg)', 'height': '22rem', 'margin-right': '15rem', 'margin-left': '0'})
+      } else if (direction === 1) {
+        $(card).css({'transform': 'rotate(10deg)', 'height': '22rem', 'margin-left': '15rem', 'margin-right': '0'})
       }
-    })
-
-    $(no).droppable({
-      drop: (event, ui) => {
-        console.log("No!")
-
-        $(event)
-      }
-    })
-
-    // Make card draggable
-    $(card).draggable({
-      refreshPositions: true,
-      revert: true,
-      scroll: false,
-      start: (event, ui) => {
-        console.log(ui)
-      },
-      stop: (event, ui) => {
-        $(card).animate(
-            {
-              deg: 0
-            },
-            {
-              duration: 100,
-              step: function (now) {
-                $(this).css({
-                  transform: 'rotate(' + now + 'deg)'
-                })
-              }
-            }
-        )
-
-        this.direction = 0
-      },
-      drag: (event, ui) => {
-        if (ui.position.left < 0 && this.direction !== -1) {
-          $(card).animate(
-              {
-                deg: -10
-              },
-              {
-                duration: 200,
-                step: function (now) {
-                  $(this).css({
-                    transform: 'rotate(' + now + 'deg)'
-                  })
-                }
-              }
-          )
-
-          this.direction = -1
-        }
-
-        else if (ui.position.left === 0 && this.direction !== 0) {
-          $(card).animate(
-              {
-                deg: 0
-              },
-              {
-                duration: 100,
-                step: function (now) {
-                  $(this).css({
-                    transform: 'rotate(' + now + 'deg)'
-                  })
-                }
-              }
-          )
-
-          this.direction = 0
-        }
-
-        else if (ui.position.left > 0 && this.direction !== 1) {
-          $(card).animate(
-              {
-                deg: 10
-              },
-              {
-                duration: 200,
-                step: function (now) {
-                  $(this).css({
-                    transform: 'rotate(' + now + 'deg)'
-                  })
-                }
-              }
-          )
-
-          this.direction = 1
-        }
-      }
-    });
+    }
   }
 }
 </script>
