8db61b0248d5f89e397136b6938816fbdfa3ee89
vvcaw <57096338+vvcaw@users.noreply.github.com>
2021-08-03 00:08:41 +0200

    Added animation for liking or disliking a song

--- frontend/components/Discover.vue
+++ frontend/components/Discover.vue
@@ -1,30 +1,33 @@
 <template>
   <div class="flex items-center w-full h-screen justify-center z-10">
     <div class="bg-black h-full grid grid-cols-2 w-full absolute items-center justify-center">
-      <div v-on:mouseenter="toggleActiveState(0)" ref="no"
-           class="bg-black border-r-2 border-solid border-opacity-20 h-full w-full flex items-center justify-center hover:bg-gray-900">
+      <div v-on:mouseenter="toggleActiveState(0)" v-on:click="yeet(0)" ref="no"
+           class="bg-black border-r-2 border-solid border-opacity-20 cursor-pointer h-full w-full flex items-center justify-center hover:bg-gray-900">
         <svg xmlns="http://www.w3.org/2000/svg" class="text-spotifyRed h-48 w-48" viewBox="0 0 20 20" fill="currentColor">
           <path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd" />
         </svg>
       </div>
-      <div v-on:mouseenter="toggleActiveState(1)" ref="yes"
-           class="bg-black border-l-1 border-solid border-opacity-20 h-full w-full flex items-center justify-center  hover:bg-gray-900">
+      <div v-on:mouseenter="toggleActiveState(1)" v-on:click="yeet(1)" ref="yes"
+           class="bg-black border-l-1 border-solid border-opacity-20 cursor-pointer h-full w-full flex items-center justify-center  hover:bg-gray-900">
         <svg xmlns="http://www.w3.org/2000/svg" class="text-spotify h-48 w-48" viewBox="0 0 20 20" fill="currentColor">
           <path fill-rule="evenodd" d="M3.172 5.172a4 4 0 015.656 0L10 6.343l1.172-1.171a4 4 0 115.656 5.656L10 17.657l-6.828-6.829a4 4 0 010-5.656z" clip-rule="evenodd" />
         </svg>
       </div>
     </div>
-    <div ref="draggable"
-         class="transition-all z-10 duration-700 ease-out cursor-move p-4 bg-white border border-opacity-25 h-72 w-64 rounded-md">
-      <h1 class="text-white">Some head</h1>
+    <!-- TODO: Add component that loads data and somehow render new ones if other one was clicked away, also add toss animation on click :) -->
+    <div ref="song"
+         class="transition-all pointer-events-none z-10 duration-700 ease-out p-4 bg-white border border-opacity-25 h-96 w-72 rounded-md">
+
     </div>
   </div>
 </template>
 
 <script>
+import Song from "./Song.vue";
 
 export default {
   name: "Discover",
+  components: {Song},
   data() {
     return {
       direction: 0
@@ -32,12 +35,22 @@ export default {
   },
   methods: {
     toggleActiveState(direction) {
-      let card = this.$refs.draggable
+      let card = this.$refs.song
 
+      // Add height modification if necessary
       if (direction === 0) {
-        $(card).css({'transform': 'rotate(-10deg)', 'height': '22rem', 'margin-right': '15rem', 'margin-left': '0'})
+        $(card).css({'transform': 'rotate(-10deg)', 'margin-right': '15rem', 'margin-left': '0'})
       } else if (direction === 1) {
-        $(card).css({'transform': 'rotate(10deg)', 'height': '22rem', 'margin-left': '15rem', 'margin-right': '0'})
+        $(card).css({'transform': 'rotate(10deg)', 'margin-left': '15rem', 'margin-right': '0'})
+      }
+    },
+    yeet(direction) {
+      let card = this.$refs.song
+
+      if(direction === 0){
+        $(card).css({'transform': 'rotate(-360deg) scale(0.1)', 'margin-right': '150%'})
+      } else if (direction === 1){
+        $(card).css({'transform': 'rotate(360deg) scale(0.1)', 'margin-left': '150%'})
       }
     }
   }
