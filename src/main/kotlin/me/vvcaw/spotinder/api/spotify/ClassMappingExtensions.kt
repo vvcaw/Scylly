9598d91692af1b51fe4b4db36d8d43549cc04fb2
vvcaw <57096338+vvcaw@users.noreply.github.com>
2021-08-11 20:16:21 +0200

    Added null check for previewUrl

--- src/main/kotlin/me/vvcaw/spotinder/api/spotify/ClassMappingExtensions.kt
+++ src/main/kotlin/me/vvcaw/spotinder/api/spotify/ClassMappingExtensions.kt
@@ -20,7 +20,7 @@ fun Track.toSongRecord() = SongRecord(
     popularity = popularity,
     explicit = isExplicit,
     openUrl = externalUrls["spotify"],
-    playUrl = previewUrl
+    playUrl = previewUrl ?: ""
 )
 
 fun TrackSimplified.toSimplifiedSongRecord() = SimplifiedSongRecord(
