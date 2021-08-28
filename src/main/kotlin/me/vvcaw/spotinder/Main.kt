29b388f7bdeceda169a227e1282178b45c6a5766
vvcaw <57096338+vvcaw@users.noreply.github.com>
2021-08-28 19:42:39 +0200

    Made project ready for open sourcing

--- src/main/kotlin/me/vvcaw/spotinder/Main.kt
+++ src/main/kotlin/me/vvcaw/spotinder/Main.kt
@@ -1,13 +1,18 @@
 package me.vvcaw.spotinder
 
+import com.beust.klaxon.Klaxon
 import me.vvcaw.spotinder.api.Api
 import me.vvcaw.spotinder.api.spotify.Spotify
+import me.vvcaw.spotinder.data.ClientData
+import java.io.File
 
 fun main(args: Array<String>) {
     // Verify if app is in production mode
     val isDevMode = args.isNotEmpty() && "DEV".equals(args[0], true)
 
-    val spotify = Spotify.getInstance()
+    val clientData = Klaxon().parse<ClientData>(File("config.json").readText(Charsets.UTF_8)) ?: throw Exception("No config file found!")
+
+    val spotify = Spotify.getInstance(clientData)
 
     Api(spotify, isDevMode, 7000)
 }
